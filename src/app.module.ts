0e22ec1ea2e03c798a33f75a94fd89877726034f
nusratalam123 <nusratalam975@gmail.com>
2025-02-08 01:18:17 +0600

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { AirQualityModule } from './air-quality/air-quality.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, AirQualityModule],
   controllers: [AppController],
   providers: [AppService],
 })

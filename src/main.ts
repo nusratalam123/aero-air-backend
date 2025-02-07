3eae4144b3c19b3e06345fc96e18e62c870237da
nusratalam123 <nusratalam975@gmail.com>
2025-02-07 12:26:28 +0600

    updated main function

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Allow cookies and credentials if needed
+  });
   await app.listen(process.env.PORT || 7000);
 }
 bootstrap();

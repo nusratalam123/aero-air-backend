b1832ce3af7759f68974a100167067d55f421897
nusratalam123 <nusratalam975@gmail.com>
2025-02-08 18:36:56 +0600

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AirQualityModule } from './air-quality/air-quality.module';
+import { PrismaService } from './prisma/prisma.service';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
   imports: [UserModule, AuthModule, AirQualityModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: 'auth/login', method: RequestMethod.POST },
+        { path: 'auth/signup', method: RequestMethod.POST }
+      )
+      .forRoutes('*'); // Apply to all other routes
+  }
+}

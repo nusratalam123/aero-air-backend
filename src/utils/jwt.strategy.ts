73bf61ac9ff0fae50eb3e387458c0f5afc98a0f7
nusratalam123 <nusratalam975@gmail.com>
2025-02-08 18:36:21 +0600

    update jwt strategy page

--- src/utils/jwt.strategy.ts
+++ src/utils/jwt.strategy.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../prisma/prisma.service'; // import PrismaService to access the database
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET || 'my-secret-key',
@@ -12,6 +13,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: { userId: string }) {
-    return { userId: payload.userId };
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.userId },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return {data:user};
   }
 }
